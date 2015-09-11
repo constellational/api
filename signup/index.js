a34813df1f39788f9a419f71e3ca4f6750831cb1
Arpith Siromoney <arpith@feedreader.co>
2015-09-11 20:09:43 +0530

    move signup to its own lambda function

---
+++ signup/index.js
@@ -0,0 +1,62 @@
+var crypto = require('crypto');
+var base64url = require('base64-url');
+var Promise = require('bluebird');
+var bcrypt = Promise.promisifyAll(require('bcryptjs'));
+var AWS = require('aws-sdk');
+var s3 = new AWS.S3();
+Promise.promisifyAll(Object.getPrototypeOf(s3));
+
+function getObj(bucket, key) {
+  console.log("Going to get " + key + " from " + bucket);
+  return s3.getObjectAsync({Bucket: bucket, Key: key}).then(function(data) {
+    var s = new Buffer(data.Body).toString();
+    return JSON.parse(s);
+  });
+}
+
+function putJSON(bucket, key, obj) {
+  console.log("Going to put " + key + " into " + bucket);
+  return s3.putObjectAsync({Bucket: bucket, Key: key, Body: JSON.stringify(obj), ContentType: 'application/json'}); 
+}
+
+function randomString() {
+  var shasum = crypto.createHash('sha1');
+  shasum.update(Math.random().toString());
+  return base64url.escape(shasum.digest('base64'));
+}
+
+
+function checkAvailable(username) {
+  console.log("Going to check if " + username + " is available");
+  var bucket = 'constellational-meta';
+  return new Promise(function(resolve, reject) {
+    getObj(bucket, username).then(function(obj) {
+      reject('Unavailable');
+    }).catch(function(err) {
+      if (err.code === 'NoSuchKey') resolve('Available');
+      else reject(err);
+    });
+  });
+}
+
+function signup(username) {
+  console.log("Going to sign " + username + " up");
+  var bucket = 'constellational-meta';
+  var token = randomString();
+  return checkAvailable(username).then(function() {
+    console.log("Going to bcrypt token");
+    return bcrypt.hashAsync(token, 10);
+  }).then(function(hash) {
+    console.log("Going to store bcrypted token");
+    return putJSON(bucket, username, {hash: hash});
+  }).then(function() {
+    console.log("Going to return username and token");
+    return {username: username, token: token};
+  });
+}
+
+exports.handler = function(event, context) {
+  console.log("Started");
+  console.log(event);
+  signup(event.username).then(context.succeed).catch(context.fail);
+};
