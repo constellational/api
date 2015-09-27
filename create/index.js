f708036a400d5b428bd7a1642e645bda5b56bcaf
Arpith Siromoney <arpith@feedreader.co>
2015-09-27 18:33:34 +0530

    post key is timestamp + post id

--- create/index.js
+++ create/index.js
@@ -45,7 +45,7 @@ function create(username, token, post) {
     post.created = new Date().toISOString();
     post.updated = post.created;
     if (!post.id) post.id = randomString();
-    var key = post.created + randomString();
+    var key = post.created + post.id;
     return putJSON(bucket, username + '/' + key, post).then(function(data) {
       post.key = key;
       post.url = key + '?VersionId=' + data.VersionId;
