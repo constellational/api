b625a83cba612a2ad99c2c2671e541f1f34e6468
Arpith Siromoney <arpith@feedreader.co>
2015-10-10 12:30:39 +0530

    basic code to send email using mandrill

--- signin/index.js
+++ signin/index.js
@@ -8,6 +8,7 @@ var s3 = new AWS.S3();
 Promise.promisifyAll(Object.getPrototypeOf(s3));
 var mandrill = require('mandrill-api/mandrill');
 var mandrill_client = new mandrill.Mandrill(process.env.MANDRILL_API_KEY);
+Promise.promisifyAll(Object.getPrototypeOf(mandrill_client));
 
 function getObj(bucket, key) {
   console.log("Going to get " + key + " from " + bucket);
@@ -39,87 +40,24 @@ function checkEmail(username, email) {
   });
 }
 
-function sendEmail(username, email) {
-/*
+function sendEmail(username, email, token) {
+  var escapedToken = base64url.escape(JSON.stringify(token));
+  var escapedEmail = base64url.escape(email);
+  var link = APP_URL + '/signin?username=' + username + '&email=' + escapedEmail + '&token=' + escapedToken;
   var message = {
-    "html": "<p>Example HTML content</p>",
-    "text": "Example text content",
-    "subject": "example subject",
-    "from_email": "message.from_email@example.com",
-    "from_name": "Example Name",
-    "to": [{
-            "email": "recipient.email@example.com",
-            "name": "Recipient Name",
-            "type": "to"
-        }],
-    "headers": {
-        "Reply-To": "message.reply@example.com"
-    },
-    "important": false,
-    "track_opens": null,
-    "track_clicks": null,
-    "auto_text": null,
-    "auto_html": null,
-    "inline_css": null,
-    "url_strip_qs": null,
-    "preserve_recipients": null,
-    "view_content_link": null,
-    "bcc_address": "message.bcc_address@example.com",
-    "tracking_domain": null,
-    "signing_domain": null,
-    "return_path_domain": null,
-    "merge": true,
-    "merge_language": "mailchimp",
-    "global_merge_vars": [{
-            "name": "merge1",
-            "content": "merge1 content"
-        }],
-    "merge_vars": [{
-            "rcpt": "recipient.email@example.com",
-            "vars": [{
-                    "name": "merge2",
-                    "content": "merge2 content"
-                }]
-        }],
-    "tags": [
-        "password-resets"
-    ],
-    "subaccount": "customer-123",
-    "google_analytics_domains": [
-        "example.com"
-    ],
-    "google_analytics_campaign": "message.from_email@example.com",
-    "metadata": {
-        "website": "www.example.com"
-    },
-    "recipient_metadata": [{
-            "rcpt": "recipient.email@example.com",
-            "values": {
-                "user_id": 123456
-            }
-        }],
-    "attachments": [{
-            "type": "text/plain",
-            "name": "myfile.txt",
-            "content": "ZXhhbXBsZSBmaWxl"
-        }],
-    "images": [{
-            "type": "image/png",
-            "name": "IMAGECID",
-            "content": "ZXhhbXBsZSBmaWxl"
-        }]
-};
-var async = false;
-var ip_pool = "Main Pool";
-var send_at = "example send_at";
-mandrill_client.messages.send({"message": message, "async": async, "ip_pool": ip_pool, "send_at": send_at}, function(result) {
-    console.log(result);
-}, function(e) {
-    // Mandrill returns the error as an object with name and message keys
-    console.log('A mandrill error occurred: ' + e.name + ' - ' + e.message);
-    // A mandrill error occurred: Unknown_Subaccount - No subaccount exists with the id 'customer-123'
-});
-*/
+    html: '<p>Hi there!</p><p>Click <a href=' + link + '>here to sign in</a></p>',
+    subject: 'Sign in to Constellational',
+    from_email: 'signin@constellational.com',
+    from_name: 'Constellational Sign In",
+    to: [{
+      email: email,
+      type: 'to'
+    }],
+    headers: {
+      Reply-To: 'arpith@constellational.com'
+    }
+  };
+  return mandrill_client.messages.sendAsync({"message": message});
 }
 
 function signin(username, email) {
@@ -141,7 +79,7 @@ function signin(username, email) {
     return putJSON(bucket, username, user);
   }).then(function() {
     console.log("Going to send email");
-    return {username: username, token: token};
+    return sendEmail(username, email, token);
   });
 }
 
